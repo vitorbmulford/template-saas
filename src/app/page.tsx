662908ecc6f62da9ec6263c91181394b0b165d79
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2025-04-15 17:29:49 -0300

    pastas iniciais

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">hello word</h1>
+    </div>
+  );
+}
