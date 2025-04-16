b4c7f7bab0e8dc4ff87cb4e7987d42b5b93290a5
vitorbmulford <vitorbmulford@vitor-dev.vitor-dev>
2025-04-16 18:06:55 -0300

    tudo funcionando basico aula 1

--- src/app/actions/handle-auth.ts
+++ src/app/actions/handle-auth.ts
@@ -1,15 +1,8 @@
 "use server";
 
-import { auth, signIn, signOut } from "@/app/lib/auth";
+import { signIn } from "@/app/lib/auth";
 
 export async function handleAuth() {
-  const session = await auth();
-
-  if (session) {
-    return await signOut({
-      redirectTo: "/",
-    });
-  }
   await signIn("google", {
     redirectTo: "/dashboard",
   });
